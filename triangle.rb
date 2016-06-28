70d96fed20da10ff47a8240af7c9b2ee65f00c29
Matthew Casiro <mattcasiro@gmail.com>
2016-06-28 13:49:24 -0700

    scaled triangle (test 2)

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  x, y, z = [a, b, c].sort
+
+  if [a, b, c].min <= 0 or x + y <= z
+    raise TriangleError.new
+  end
   if a == b and b == c 
     :equilateral
   elsif a == b or a == c or b == c 
